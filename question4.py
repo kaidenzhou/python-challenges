a165df6f4c83fc53ab719a6f670aa8b32721b2e8
Kaiden Zhou <kzhou@mac.lan>
2025-12-31 10:26:01 -0600

    First commit

---
+++ question4.py
@@ -0,0 +1,15 @@
+# Q4.1
+peanut_cookies = 10
+chocolate_cookies = 15
+peanut_cookie_price = 3
+chocolate_cookie_price = 2
+
+total_income = peanut_cookies * peanut_cookie_price + chocolate_cookies * chocolate_cookie_price
+print("The total income is", total_income, "dollars.")
+
+# Q4.2
+peanut_cookie_price = peanut_cookie_price + 1
+chocolate_cookie_price = chocolate_cookie_price + 2
+
+new_total_income = peanut_cookies * peanut_cookie_price + chocolate_cookies * chocolate_cookie_price
+print("The new total income is", new_total_income, "dollars.")
